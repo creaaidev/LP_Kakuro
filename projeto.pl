6dd4a331cfe3a75a19a7c3fb9f0041df6fa75764
creaai <martimrosam@tecnico.ulisboa.pt>
2021-04-24 16:31:00 +0100

    Add permutacoes_soma
    And some ideas for espaco_fila

--- projeto.pl
+++ projeto.pl
@@ -1,5 +1,7 @@
 % Martim Rosa Monis ist199281 :pray: :pavaobless:.
 
+% 2/20 Completeness Ratio
+
 % gets stuff provided by the teachers.
 :- [codigo_comum, puzzles_publicos].
 
@@ -11,6 +13,14 @@ soma_lista([P | R], Res) :- soma_lista(R, NRes), Res is P + NRes.
 comb_soma_valida(N, Els, Soma, L) :- combinacao(N, Els, L), soma_lista(L, NSoma), NSoma =:= Soma.
 
 % returns a sorted list with all the possible combinations of N numbers that have sum equal to N.
-combinacoes_soma(N, Els, Soma, Combs) :- bagof(Comb, comb_soma_valida(N, Els, Soma, Comb), Combs), !.
+combinacoes_soma(N, Els, Soma, Combs) :- bagof(Comb, comb_soma_valida(N, Els, Soma, Comb), Combs).
 
-permutacoes_soma(N, Els, Soma, Perms)
+% returns a sorted list with all possible permutations (...), also gaspa is love, gaspa is life 
+permutacoes_soma(N, Els, Soma, Perms) :- combinacoes_soma(N, Els, Soma, Combs), findall(Perm, (member(X, Combs), permutation(X, Perm)), UPerms), sort(1, @<, UPerms, Perms).
+
+% espaco_fila(Fila, Esp, H_V) , espaco(Soma, [variaveis]), unfinished mess
+
+espaco_fila(Fila, Esp, H_V) :- esp_fila_aux(Fila, Aux, Esp, H_V).
+esp_fila_aux([], Aux, Esp, H_V) :- member(Esp, Aux).
+esp_fila_aux([P | R], 
+esp_fila_aux([[V | _] | R], [Aux | E], Esp, v) :- Aux = espaco(V, Vars).
